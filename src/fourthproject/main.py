16fd4279ac7ed29a4be8b7d898cf5e3cbe9345e9
GITSangWoo <dogcenta@gmail.com>
2024-11-14 14:27:25 +0900

    done

--- src/fourthproject/main.py
+++ src/fourthproject/main.py
@@ -38,13 +38,18 @@ with st.form(key="docker"):
     with col2:
         scale_in = st.form_submit_button(label='manual scale in')
     if scale_out:
-        scaleout(cnt)
         result0,result1,result2 = checkAll()
-        st.write(cnt,"개","scale out 되었습니다.","총 컨테이너 수:",result1,"개 입니다.")
+        if (result1+int(cnt)) <5:
+            scaleout(cnt)
+            dockercnt=result1+int(cnt)
+            st.write(cnt,"개","scale out 되었습니다.","총 컨테이너 수:",dockercnt,"개 입니다.")
+        else :
+            st.write("4개 이하만 생성 가능합니다.")
     if scale_in:
         result0,result1,result2 = checkAll()
         if result1 >1:
             scalein(cnt)
-            st.write(cnt,"개","scale out 되었습니다.","총 컨테이너 수:",result1,"개 입니다.")
+            dockercnt=result1-int(cnt)
+            st.write(cnt,"개","scale out 되었습니다.","총 컨테이너 수:",dockercnt,"개 입니다.")
         else : 
             st.write("현재 운용중인 컨테이너가 1개로 이미 최소입니다.")
